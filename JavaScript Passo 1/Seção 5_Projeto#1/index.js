dc505f24bcb7641fd7f3b6c95a703481e2ee9c2c
caiocipriano <caio.silvax@hotmail.com>
2022-05-31 10:39:06 -0300

    31/05

--- JavaScript Passo 1/Seção 5_Projeto#1/index.js
+++ JavaScript Passo 1/Seção 5_Projeto#1/index.js
@@ -5,28 +5,13 @@ const app = express();
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
-
-app.get("/:nome?/:lang?", (req,res)=>{
-    const nome = req.params.nome
-    const lang = req.params.lang
-    const carrinho = [
-        {produto:"S20Fe", preco:2100.50},
-        {produto:"SmartDex",  preco:400},
-        {produto:"SmartCooler", preco:60}
-        ]
-    if(nome && lang){
-        res.render('index.ejs',{
-            nome:nome,
-            lang:lang,
-            empresa:"Any",
-            msg: false,
-            carrinho: carrinho
-        })
-    }else{
-        res.send("Erro!")
-    }
+app.get("/", (req,res)=>{
+    res.render('index')
 })
 
+app.get("/ask", (req,res)=>{
+    res.render('ask')
+})
 
 
 app.listen(8080, erro=>{
