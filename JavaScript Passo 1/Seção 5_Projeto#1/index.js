2550b8989f1a50c8880d1ccb6199ce5d9803e62e
caiocipriano <caio.silvax@hotmail.com>
2022-05-31 13:58:50 -0300

    31/05

--- JavaScript Passo 1/Seção 5_Projeto#1/index.js
+++ JavaScript Passo 1/Seção 5_Projeto#1/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
 
+const bodyParser= require('body-parser')
+
 //Apontando para o Express usar o EJS   
 app.set('view engine', 'ejs')
 app.use(express.static('public'));
 
+//BodyParser
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json())
+
+//Rotas
 app.get("/", (req,res)=>{
     res.render('index')
 })
@@ -13,7 +20,17 @@ app.get("/ask", (req,res)=>{
     res.render('ask')
 })
 
+app.post("/saveAsk", (req,res)=>{
+    const title = req.body.title
+    const description = req.body.description
+    res.send("Paasou!"+title+description)
+})
 
+
+
+
+
+//Rodando
 app.listen(8080, erro=>{
     if(erro){
         console.log("Erro!")
