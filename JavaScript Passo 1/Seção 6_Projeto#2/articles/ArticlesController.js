33fac3c64efd539bf9e87b82b03cc44d3126eae9
caiocipriano <caio.silvax@hotmail.com>
2022-06-02 21:14:52 -0300

    02/06

--- JavaScript Passo 1/Seção 6_Projeto#2/articles/ArticlesController.js
+++ JavaScript Passo 1/Seção 6_Projeto#2/articles/ArticlesController.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+router.get("/admin/articles/new",(req,res)=>{
+    res.render("admin/articles/new")
+})
 
 module.exports=router
