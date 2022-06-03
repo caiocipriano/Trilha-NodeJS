0ac00f3c2341e9606219b0661c4ff28bee25108b
caiocipriano <caio.silvax@hotmail.com>
2022-06-02 22:55:28 -0300

    02/06

--- JavaScript Passo 1/Seção 6_Projeto#2/articles/ArticlesController.js
+++ JavaScript Passo 1/Seção 6_Projeto#2/articles/ArticlesController.js
@@ -1,8 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../categories/Category')
+const Article = require('./Article')
+const slugify = require("slugify")
+
+router.get("/admin/articles",(req,res)=>{
+        res.render("admin/articles/index")
+})
 
 router.get("/admin/articles/new",(req,res)=>{
-    res.render("admin/articles/new")
+    Category.findAll().then(categories =>{
+        res.render("admin/articles/new",{categories:categories})
+    })
+})
+
+router.post("/articles/save", (req,res)=>{
+    const title = req.body.title
+    const body = req.body.body
+    const category =req.body.category
+
+    Article.create({
+        title:title,
+        slug:slugify(title),
+        body:body,
+        categoryId:category
+    }).then(()=>{
+        res.redirect("/admin/articles")
+    }).catch(()=>{
+        res.render("/")
+    })
 })
 
 module.exports=router
--- JavaScript Passo 1/Seção 6_Projeto#2/database/database.js
+++ JavaScript Passo 1/Seção 6_Projeto#2/database/database.js
@@ -1,7 +1,8 @@
 const { Sequelize } = require('sequelize')
 const connection = new Sequelize('blog_express','root','admin',{
     host:'localhost',
-    dialect:'mysql'
+    dialect:'mysql',
+    timezone:"-03:00"
 })
 
 module.exports=connection;
